5dcf5918644292f92dc3552397b581e388e25d9f
muhammedatef98 <31451355+muhammedatef98@users.noreply.github.com>
2025-12-12 20:25:08 -0500

    Add Web3Forms access key for email notifications

--- client/src/pages/ServiceRequest.tsx
+++ client/src/pages/ServiceRequest.tsx
@@ -141,7 +141,7 @@ export default function ServiceRequest() {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            access_key: 'YOUR_WEB3FORMS_KEY', // You'll need to get this from web3forms.com
+            access_key: 'd3ff12a4-e013-473f-8730-9d5760059a64',
             subject: `🔔 حجز جديد - Fixatee`,
             from_name: 'Fixatee Website',
             to: 'fixate01@gmail.com',
